893ac2fee97d2eaaf0f9effc00f91f026c5980ff
Sujeong Park <sujpark@c4r5s7.42seoul.kr>
2022-04-06 11:18:03 +0900

    feat : 재시작 기능

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import BaseBallGame from "./BaseBallGame.js"
 
 export default class App {
 	constructor() {
-		this.$form = document.getElementById('input-form')
-		this.$input = document.getElementById('user-input')
-		this.$button = document.getElementById('submit')
-		this.$result = document.getElementById('result')
+		this.$form = document.getElementById('input-form');
+		this.$input = document.getElementById('user-input');
+		this.$button = document.getElementById('submit');
+		this.$result = document.getElementById('result');
+		this.$restart = document.getElementById('game-restart-button');
+		this.$restart.style.display = "none";
 		const baseBallGame = new BaseBallGame();
 		
 		this.$form.addEventListener('submit', (e) => {
 			e.preventDefault();
-			const inputValue = this.$input.value
-			const result = baseBallGame.takeUserNumber(inputValue)
+			const inputValue = this.$input.value;
+			const result = baseBallGame.takeUserNumber(inputValue);
 			console.log(result);
-			this.$result.innerText = result.message;
+			if (result.result === "error") alert(result.message);
+			else {
+				if (result.result === "success") this.$restart.style.display = "inline-block"
+				this.$result.innerText = result.message;
+			}
 			this.$input.value='';
+		})
+
+		this.$restart.addEventListener('click', (e) => {
+			this.$result.innerText = '';
+			this.$restart.style.display = "none";
+			baseBallGame.start();
 		})
 	}
 }
